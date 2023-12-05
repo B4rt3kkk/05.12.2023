95bdb2dd5004d73a7cd0a351aa3a5035507f8e30
FilipZembrzuski <fzembrzuski@gmail.com>
2023-12-05 10:29:38 +0100

    Koniec

---
+++ Zadanie1/Zadanie1/Zadanie1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int rozmiar;
+
+	cout << "Podaj rozmiar tablicy imion = "; cin >> rozmiar;
+
+	string* imiona = new string[rozmiar];
+
+	for (size_t i = 0; i < rozmiar; i++)
+	{
+		cout << "Podaj imie " << i+1 << " = "; cin >> imiona[i];
+	}
+
+	for (size_t i = 0; i < rozmiar; i++)
+	{
+		if (i == rozmiar) cout << imiona[i];
+		else cout << imiona[i] << ", ";
+	}
+
+	delete[] imiona;
+
+	return EXIT_SUCCESS;
+}
